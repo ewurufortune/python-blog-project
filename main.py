add770f468228d2d193dbd800deb13ebf0081e7e
Ewuru Freeborn <109674539+15sxope@users.noreply.github.com>
2023-10-22 08:08:33 +0100

    commented in db.create_all()

--- main.py
+++ main.py
@@ -103,8 +103,8 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
 
 
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 
 
 # Create an admin-only decorator
